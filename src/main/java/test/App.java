77dbda016d7ec9a73091b9c26c780372a6a52698
cedricbou <cedricb1101@gmail.com>
2015-03-18 22:34:39 +0100

    updated procfile to run App, getting port from ENV

--- src/main/java/test/App.java
+++ src/main/java/test/App.java
@@ -4,14 +4,21 @@ import org.apache.cxf.interceptor.LoggingInInterceptor;
 import org.apache.cxf.interceptor.LoggingOutInterceptor;
 import org.apache.cxf.jaxws.JaxWsServerFactoryBean;
 
+import com.google.common.base.Optional;
+
 public class App {
 
 	public static void main(String[] args) throws Exception {
-	
+		System.out.println("Starting server...");
+		
 		MyServiceImpl implementor = new MyServiceImpl();
 		JaxWsServerFactoryBean svrFactory = new JaxWsServerFactoryBean();
 		svrFactory.setServiceClass(MyService.class);
-		svrFactory.setAddress("http://localhost:9000/service");
+		
+		final String address = "http://localhost:" + Optional.fromNullable(System.getenv("PORT")).or("9000") + "/";
+		System.out.println("Binding on " + address + "...");
+		svrFactory.setAddress(address);
+		
 		svrFactory.setServiceBean(implementor);
 		svrFactory.getInInterceptors().add(new LoggingInInterceptor());
 		svrFactory.getOutInterceptors().add(new LoggingOutInterceptor());
